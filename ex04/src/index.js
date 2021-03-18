8a100e1b3c89957ce468dd1dab85c1b1eaff0e70
Teska98 <63234508+Teska98@users.noreply.github.com>
2021-03-19 00:55:41 +0100

    Create index.js

---
+++ ex04/src/index.js
@@ -0,0 +1,13 @@
+// Create an monitorsListArray array here
+var monitorsListArray = ["Apple", "Peach", "Berry"];
+// End of monitorsListArray array
+
+function myMonitorsFunction(arr) {
+ var newMonitorsList = [...arr];
+  // Only change code below this line
+  
+  // Only change code above this line
+}
+
+console.log(myMonitorsFunction(arr));
+module.exports = myMonitorsFunction;
